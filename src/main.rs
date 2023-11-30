453b6bdbbb4468b40acc942f0e342f46dfa9b553
imperi13 <imperi.night@gmail.com>
2023-11-30 16:06:34 +0900

    impl clap template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+use clap::Subcommand;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(subcommand)]
+    subcommand: Subcommands,
+}
+
+#[derive(Subcommand)]
+enum Subcommands {
+    /// test all case in in_folder & output to out_folder
+    TestAll {
+        in_folder: PathBuf,
+        out_folder: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match args.subcommand {
+        Subcommands::TestAll {
+            in_folder,
+            out_folder,
+        } => println!("{:?} {:?}", in_folder, out_folder),
+    }
 }
